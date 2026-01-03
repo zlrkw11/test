4f8c4ba1592a9abc3abd1e706044471255af2c4d
ZL <ray040406@gmail.com>
2026-01-04 01:19:03 +1300

    feat: id & generated value by default

--- src/main/java/com/example/test/model/BookEntity.java
+++ src/main/java/com/example/test/model/BookEntity.java
@@ -2,6 +2,8 @@ package com.example.test.model;
 
 import jakarta.persistence.Table;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +12,8 @@ import lombok.Setter;
 @Entity
 @Table(name = "book")
 public class BookEntity {
+    @Id
+    @GeneratedValue
     private Long id;
     private String title;
     private String author;
