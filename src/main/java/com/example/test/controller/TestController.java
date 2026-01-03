01579b3384515570b1126152374fb2a7685f220b
ZL <ray040406@gmail.com>
2026-01-04 01:13:58 +1300

    feat: test controller

--- src/main/java/com/example/test/controller/TestController.java
+++ src/main/java/com/example/test/controller/TestController.java
@@ -1,5 +1,14 @@
 package com.example.test.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    @GetMapping("/")
+    public String getMethodName() {
+        return "Hello World";
+    }
 
 }
