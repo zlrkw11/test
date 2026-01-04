7686a3b2165673edd8b46e6546e1610993be8b69
ZL <ray040406@gmail.com>
2026-01-04 15:36:34 +1300

    feat: book controller, createBook function

---
+++ README.md
@@ -0,0 +1,6 @@
+# Initilize the repo by using springboot initializr
+https://start.spring.io/
+
+## dependency injection (DI)
+final: unchangable
+
--- src/main/java/com/example/test/controller/BookController.java
+++ src/main/java/com/example/test/controller/BookController.java
@@ -1,5 +1,23 @@
 package com.example.test.controller;
 
-public class BookController {
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.test.model.BookEntity;
+import com.example.test.repository.BookRepository;
+
+@RestController
+public class BookController {
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public BookEntity createBook(String title, String author, String isbn) {
+        BookEntity bookEntity = new BookEntity();
+        bookEntity.setTitle(title);
+        bookEntity.setAuthor(author);
+        bookEntity.setIsbn(isbn);
+        return bookRepository.save(bookEntity);
+    }
 }
